fe04869c555ef30d421b36833689496a45d34879
thebobak <bshafiei@gmail.com>
2015-04-12 22:57:57 -0400

    update

--- mkdir-single.pl
+++ mkdir-single.pl
@@ -4,8 +4,8 @@ use File::Copy;
 
 my $root_folder = "/Users/bobak/Dropbox/_CHEC/2014-2015/_Q3/P4";
 my @periods = ("Student Folders");
-my $sourcefile = "/Users/bobak/Desktop/Lab - Photo Alterations.psd";
-my $destinationfile = "Lab - Photo Alterations.psd";
+my @sourcefiles = ("/Users/bobak/Desktop/Entry Slip.docx","/Users/bobak/Desktop/Instructions.docx");
+my @destinationfiles = ("Entry Slip.docx", "Instructions.docx");
 
 ## Change into root folder ##
 
@@ -21,11 +21,11 @@ print "--------------------------------------\n";
 print "         Create a New Folder          \n";
 print "--------------------------------------\n";
 print "\n\n\n";
-print "Type a name for the folder:\n";
+##print "Type a name for the folder:\n";
 
-my $input = <STDIN>;
-chomp $input;
-my $newfoldername = $input;
+##my $input = <STDIN>;
+##chomp $input;
+my $newfoldername = "FINAL PROJECT";
 
 
 ## Create the Folder in Each Period ##
@@ -33,16 +33,21 @@ my $newfoldername = $input;
 foreach $period (@periods) {
 
     chdir($period) or die "folder doesn't exist\n\n";
-    @files = <*>;
-    foreach $file (@files) {
+    @folders = <*>;
+    foreach $folder (@folders) {
         
-        if (-d $file)  {
-            chdir($file);
+        if (-d $folder)  {
+            chdir($folder);
             mkdir($newfoldername);
-            copy($sourcefile, $destinationfile) or die "Copy failed: $!";
+            chdir($newfoldername);
+
+            foreach $sourcefile (@sourcefiles) {
+                copy($sourcefile, substr($sourcefile, 21)) or die "Copy failed: $!";
+            }
             chdir("../");
         }
         
+        chdir("../");
         
     }
     
